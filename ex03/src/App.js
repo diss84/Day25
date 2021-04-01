9cbca60d0079f4e0aeb01ef717183dd7b1e501ba
Mirela <adisserdarevic@gmail.com>
2021-04-01 23:30:18 +0200

    Added folder ex03

---
+++ ex03/src/App.js
@@ -0,0 +1,13 @@
+//App.js/ex03
+import './App.css';
+import GetRequestErrorHandling from './GetRequestErrorHandling';
+//function App
+function App() {
+  return (
+    <div className="App">
+      <GetRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
