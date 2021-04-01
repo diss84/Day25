356402feb018fc6d862004afd4b80fcaaec11577
Mirela <adisserdarevic@gmail.com>
2021-04-01 23:32:10 +0200

    Added folder ex05

---
+++ ex05/src/App.js
@@ -0,0 +1,12 @@
+//App.js
+import './App.css';
+import PostRequest from './PostRequest';
+function App() {
+  return (
+    <div className="App">
+    <PostRequest />
+    </div>
+  );
+}
+
+export default App;
