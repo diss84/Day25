96e64937fffd10932d11e61687484a5f28077f41
Mirela <adisserdarevic@gmail.com>
2021-04-01 23:28:24 +0200

    Added folder ex01

---
+++ ex01/src/App.js
@@ -0,0 +1,12 @@
+//App.js/GetRequest
+import './App.css';
+import GetRequest from './GetRequest';
+//function App
+function App() {
+  return (
+    <div className="App">
+      <GetRequest />
+    </div>
+  );
+}
+export default App; // export App
